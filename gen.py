653c83aef91e4937554bfb0d40012a6b4f0e508a
Nelson Minar <nelson@monkey.org>
2013-02-23 09:24:58 -0800

    comments

--- gen.py
+++ gen.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python
 
+"""Convert Ben Fry's zip decode database to
+GeoJSON and a tab separated file.
+"""
+
 import gzip, json
 from contextlib import closing
 
 features = []
 fields = []
 
+# Read in Ben Fry's database
+# Downloaded from http://benfry.com/zipdecode/zips.gz
 for l in gzip.open("zips.gz"):
     zipcode = l[0:5]
     lat = float(l[6:16])
     lon = float(l[17:28])
+
+    # Simple data
     fields.append((lon, lat, zipcode))
+
+    # GeoJSON's loquacious feature schema
     features.append({ "type": "Feature",
                       "geometry": {
                         "type": "Point",
@@ -19,10 +29,13 @@ for l in gzip.open("zips.gz"):
                       "id": zipcode,
                       "properties": None })
 
+# Emit GeoJSON
 collection = { "type": "FeatureCollection",
                "features": features }
 with closing(file("zips.geojson", "w")) as fp:
     json.dump(collection, fp)
+
+# Emit TSV file
 with closing(file("zips.tsv", "w")) as fp:
     fp.write("lon\tlat\tzip\n")
     for row in fields:
