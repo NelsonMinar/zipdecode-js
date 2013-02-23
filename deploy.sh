2fa3c9beff26f9188a63962168c7d5e7d5838175
Nelson Minar <nelson@monkey.org>
2013-02-23 11:45:31 -0800

    deploy one more file

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ TS=`date +%s`
 DATE=`date`
 git tag -a deploy-$TS -m "Deploy at $DATE"
 rsync -av \
-  index.html queue.min.js us-states.geojson zips.tsv \
+  index.html queue.min.js us-states.geojson zips.tsv forkme.png \
   somebits.com:/var/www/zipdecode-js/
